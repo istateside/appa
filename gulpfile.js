b5b67636d46b8773f33b9f2df8faa44936e567ad
istateside <kevin.r.fleischman@gmail.com>
2017-02-12 15:21:26 -0500

    adds script file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const gulp = require('gulp');
+const util = require('gulp-util');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const haml = require('gulp-haml');
 const livereload = require('gulp-livereload');
+const babel = require('gulp-babel');
 
 gulp.task('sass', () => {
   return gulp.src('./src/*.scss')
@@ -20,13 +22,21 @@ gulp.task('sass', () => {
 gulp.task('haml', () => {
   return gulp.src('./src/index.haml')
     .pipe(haml())
+    .on('error', util.log)
     .pipe(gulp.dest('./build'))
 });
 
-gulp.task('watch', function() {
-  livereload.listen({ reloadPage: './build/index.html' });
-  gulp.watch('./src/*.scss', ['sass']);
-  gulp.watch('./src/*.haml', ['haml']);
+gulp.task('babel', () => {
+  gulp.src('src/script.js')
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(gulp.dest('build'));
 });
 
-gulp.task('default', ['watch', 'sass', 'haml']);
+gulp.task('watch', function() {
+  livereload.listen({ reloadPage: 'build/index.html' });
+  gulp.watch('./src/*.scss', ['sass']);
+  gulp.watch('./src/*.haml', ['haml']);
+  gulp.watch('./src/*.js', ['babel']);
+});
+
+gulp.task('default', ['watch', 'sass', 'haml', 'babel']);
